0753d842c469dce61e50b05c952e56335b3bf8e0
twg0 <fruitsjh@gmail.com>
2023-05-30 23:28:28 +0900

    feat: ToDo Repository 구현 - ToDoRepository

---
+++ src/main/java/likelion/todolist/repository/ToDoRepository.java
@@ -0,0 +1,36 @@
+package likelion.todolist.repository;
+
+import jakarta.persistence.EntityManager;
+import likelion.todolist.entity.ToDo;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Repository;
+
+import java.util.List;
+
+@Repository
+@RequiredArgsConstructor
+public class ToDoRepository {
+
+    private final EntityManager em;
+
+    public void save(ToDo todo) {
+        if (todo.getId() == null) {
+            em.persist(todo);
+        } else {
+            em.merge(todo);
+        }
+    }
+
+    public void delete(ToDo todo) {
+        em.remove(todo);
+    }
+
+    public ToDo findOne(Long id) {
+        return em.find(ToDo.class, id);
+    }
+
+    public List<ToDo> findAll() {
+        return em.createQuery("select i from ToDo i", ToDo.class)
+                .getResultList();
+    }
+}
