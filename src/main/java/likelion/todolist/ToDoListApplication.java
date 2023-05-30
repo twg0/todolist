822bdfb0a39b16be138426b195ed39b13287d658
twg0 <fruitsjh@gmail.com>
2023-05-30 23:32:26 +0900

    first commit

--- src/main/java/likelion/todolist/ToDoListApplication.java
+++ src/main/java/likelion/todolist/ToDoListApplication.java
@@ -2,6 +2,8 @@ package likelion.todolist;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+import org.springframework.web.filter.HiddenHttpMethodFilter;
 
 @SpringBootApplication
 public class ToDoListApplication {
@@ -9,5 +11,4 @@ public class ToDoListApplication {
     public static void main(String[] args) {
         SpringApplication.run(ToDoListApplication.class, args);
     }
-
 }
